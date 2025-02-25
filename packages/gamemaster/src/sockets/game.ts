3c95cd55a12c8ad9dcc488babb468586d922d157
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-02-25 17:16:22 -0300

    feat: [gamemaster] implementing simple flow

--- packages/gamemaster/src/sockets/game.ts
+++ packages/gamemaster/src/sockets/game.ts
@@ -1,12 +1,18 @@
 import { Server } from 'socket.io';
+import { getGameOrNewOne, Player, Games } from '../game.js';
 
 export function addGameNamespace(server: Server): Server {
 
   //=> "V1StGXR8_Z5jdHi6B2myT"
   const gameNsp = server.of(/^\/game\/[a-zA-Z0-9_\-]+$/);
+
   gameNsp.on('connection', (socket) => {
-    console.log(`[game:=${socket.nsp.name}] User connection`)
+    const game = getGameOrNewOne(socket.nsp);
+    game.addPlayer(socket.id as Player)
+    console.log(`[${socket.nsp.name}] User connection`)
   });
+
+
 
   return server;
 
