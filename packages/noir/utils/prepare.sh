288ce75acbacd18525d77ab6f3ade7c74caf5032
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-03-11 18:47:43 -0300

    chore: adding package.json for noir

---
+++ packages/noir/utils/prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CIRCUITS=(
+    "answers_updates"
+    "blinded_answers"
+    "combine_queries"
+    "initial_deploys"
+    "keypair"
+    "offline_queries"
+    "reports_updates"
+)
+
+BUILD_DIR="circuits"
+DEST_DIR="dist/circuits"
+
+
+mkdir -p "$DEST_DIR"
+for circuit in "${CIRCUITS[@]}"
+do
+  cp "$BUILD_DIR/$circuit/target/${circuit}.json" "$DEST_DIR/"
+done
