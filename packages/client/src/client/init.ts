67278a98e744ce5e13cde6e38c81518805396b63
Daffy Hellman <daffy@fatsolutions.xyz>
2025-04-21 13:32:20 -0300

    added user cookie management for frontend

--- packages/client/src/client/init.ts
+++ packages/client/src/client/init.ts
@@ -23,3 +23,26 @@ export async function initClient(name: string, url: string, gameId: string) {
     console.log("Could not get user token from gamemaster in /auth")
   } 
 }
+
+export async function getNewToken(name: string, url: string) { 
+  const token = await getAuthToken({name, url});
+    if (token) {
+      return token;
+    } else {
+      return null;
+    }
+}
+
+export async function connect(token: string, url: string, gameId: string) {
+  const sockets = new SocketManager({
+    serverUrl: url,
+    token: token,
+    gameId: gameId, 
+  });
+
+  await sockets.socketsReady();
+
+  const client = new GameClient(sockets.token, sockets);
+
+  await client.play();
+}
