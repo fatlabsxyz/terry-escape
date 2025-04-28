f636f3398492df1c70d57d6a9e821d236ba113a6
Daffy Hellman <daffy@fatsolutions.xyz>
2025-04-28 19:11:55 -0300

    updated packages, added zklib to cli script for testing

--- packages/client/src/cli/cli.ts
+++ packages/client/src/cli/cli.ts
@@ -3,6 +3,12 @@ import { GameClient } from "./../client/game/gameclient.js";
 import { SocketManager } from "./../client/sockets/socketManager.js";
 import { getAuthToken, AuthRequestData } from "./../utils.js";
 
+import { zklib } from "zklib";
+
+type Field = string;
+type BigNum = Field[];
+type Secret_Key = BigNum;
+type Public_Key = { key_set: BigNum[], params: any };
 
 const args = process.argv.splice(2)
 
@@ -12,8 +18,18 @@ export function passTime(ms: number): Promise<void> {
   })
 }
 
-export async function initCli() {
+export function testZkLib() {
+  const privkey: Secret_Key = ["test", "test"];
+  const pubkeyNum: BigNum = ["testpub", "testpub"];
+  const pubkey: Public_Key = { key_set: [pubkeyNum], params: null };
+  const zklibber: zklib = new zklib(0, privkey, [pubkey]);
 
+  console.log(zklibber);
+}
+
+export async function initCli() {
+    
+  testZkLib();
   const url = args[0]!;
   const name = args[1]!;
   const gameId = args[2]!;
